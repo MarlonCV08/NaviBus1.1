7567242fa99cd9c3d4bf37c11ebb015a1178ee8d
Marlon <marlonstiven0208@gmail.com>
2024-10-03 18:56:31 -0500

    camiones

--- Client/src/Components/Camion.jsx
+++ Client/src/Components/Camion.jsx
@@ -1,9 +1,11 @@
 import '../Styles/Camion.css'
+import Camion2 from '../Assets/Images/Camion2.svg'
 import CamionImg from '../Assets/Images/Camion.svg'
 export const Camion = () => {
   return (
-    <>
-        <img src={CamionImg} alt="" className="camion"/>
-    </>
+    <div className='divCamiones'>
+        <img src={Camion2} className='camion2'/>
+        <img src={CamionImg} className="camion"/>
+    </div>
   );
 };
